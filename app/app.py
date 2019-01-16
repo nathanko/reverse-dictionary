2c486aa01532fbc96bd650e9faa758f081e98446
nathanko <nathan@nathanko.com>
2019-01-16 11:02:40 -0500

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+from text_classifier import TextClassifier
+import json
+import sys
+import os
+
+clf = None
+
+app = Flask(__name__)
+
+@app.route('/api/classify')
+def classify():
+    if 'text' in request.args:
+      prediction = clf.classify(request.args['text'])
+      return jsonify({
+        'message': {'prediction': prediction},
+        })
+    else:
+      resp = jsonify({'message': "'text' was not found in request arguments."})
+      resp.status_code = 400
+      return resp
+
+if __name__ == '__main__':
+
+  print("Reading data...")
+  try:
+    filename = os.path.join(os.path.dirname(__file__), 'data/dictionary-small.json')
+    with open(filename) as f:
+      print ("Parsing data...")
+      data = json.load(f)
+      print ("Creating classifier...")
+      clf = TextClassifier(data)
+  except EnvironmentError as e:
+    print ('ERROR reading data: {}'.format(e), file=sys.stderr)
+  print("Launching web app...")
+  app.run()
---
+++ app/text_classifier.py
@@ -0,0 +1,43 @@
+import string
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def LemmatizeTokenizer(text):
+  punctTranslateDict = dict((ord(punc), None) for punc in string.punctuation)
+  text = text.lower().translate(punctTranslateDict)
+  tokens = nltk.word_tokenize(text)
+  lemmer = nltk.stem.WordNetLemmatizer()
+  return [lemmer.lemmatize(token) for token in tokens]
+
+class TextClassifier:
+
+  entries = []
+  values = []
+
+  def __init__(self, data):
+    dataX = []
+    dataY = []
+    for entry, definition in data.items():
+      # print(dataX, dataY)
+      dataX = dataX + [definition]
+      dataY = dataY + [entry]
+    self.entries = dataY
+    self.values = dataX
+
+  def classify(self, question):
+    tfidfVec = TfidfVectorizer(tokenizer=LemmatizeTokenizer, stop_words=None)
+    tfidf = tfidfVec.fit_transform(self.values + [question])
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    # print(self.values + [question])
+    # print(vals)
+    # print(vals.argsort()[0])
+    # return top numResults results in desc order
+    numResults = 4
+    numResults = min(numResults, len(self.entries))
+    indices = vals.argsort()[0][-numResults-2:-2][::-1]
+    return [{
+      "entry" : self.entries[idx],
+      "definition": self.values[idx],
+      "cosine": vals[0][idx]
+    } for idx in indices]
